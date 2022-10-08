4a8ddf0633b00243ba89dd697c4ce02678bdb628
Jowan Jomon <jowanjomon619@gmail.com>
2022-10-08 14:49:23 +0400

    First Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const verse = document.getElementById("verse");
+const author = document.getElementById("author");
+
+(function () {
+  $(document).ready(function () {
+    getVerse();
+  });
+
+  $("button").click(function () {
+    getVerse();
+  });
+
+  var getVerse = function () {
+    $("#spinner").show();
+    $.ajax({
+      url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback",
+      crossDomain: true,
+      dataType: "jsonp",
+      success: function (result) {
+        verse.innerHTML = result[0].text;
+        author.innerHTML = `${result[0].bookname} ${result[0].chapter} :${result[0].verse}`;
+      },
+    });
+  };
+})();
